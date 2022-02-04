8b4076184bce89cef414ade3870be526bccac7a2
Alexander Cyon <alex.cyon@gmail.com>
2022-02-04 18:24:58 +0100

    Add PrivateKey generation tests

---
+++ Tests/K1Tests/TestCases/Keys/PrivateKeyGenerationTests.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexander Cyon on 2022-02-04.
+//
+
+import Foundation
+import K1
+import XCTest
+
+final class PrivateKeyGenerationTests: XCTestCase {
+
+    func testGenerationWorks() throws {
+        XCTAssertNoThrow(try PrivateKey.generateNew())
+    }
+    
+    func testRandom() throws {
+        // The probability of two keys being identical is approximately: 1/2^256
+        XCTAssertNotEqual(try PrivateKey.generateNew(), try PrivateKey.generateNew())
+    }
+    
+}
