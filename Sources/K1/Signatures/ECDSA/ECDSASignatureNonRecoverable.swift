7b2eba847e70788a23292b01c4f239793fb903ea
Alexander Cyon <alex.cyon@gmail.com>
2023-03-19 21:05:10 +0100

    WIP

--- Sources/K1/Signatures/ECDSA/ECDSASignatureNonRecoverable.swift
+++ Sources/K1/Signatures/ECDSA/ECDSASignatureNonRecoverable.swift
@@ -11,39 +11,31 @@ import FFI
 
 public struct ECDSASignatureNonRecoverable: Sendable, Hashable {
     
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.wrapped.withUnsafeBytes { lhsBytes in
-            rhs.wrapped.withUnsafeBytes { rhsBytes in
-                safeCompare(lhsBytes, rhsBytes)
-            }
-        }
-    }
-    public func hash(into hasher: inout Hasher) {
-        wrapped.withUnsafeBytes {
-            hasher.combine(bytes: $0)
-        }
-    }
-    
     typealias Wrapped = Bridge.ECDSA.NonRecovery.Wrapped
+    
     internal let wrapped: Wrapped
+    
     init(wrapped: Wrapped) {
         self.wrapped = wrapped
     }
-    
+}
+
+// MARK: Inits
+extension ECDSASignatureNonRecoverable {
     public init(rawRepresentation: some DataProtocol) throws {
         // FIXME: Needed?
-//        guard
-//            rawRepresentation.count == Self.byteCount
-//        else {
-//            throw Bridge.Error.incorrectByteCountOfRawSignature
-//        }
-//
-//        self.rawRepresentation = Data(rawRepresentation)
+        //        guard
+        //            rawRepresentation.count == Self.byteCount
+        //        else {
+        //            throw Bridge.Error.incorrectByteCountOfRawSignature
+        //        }
+        //
+        //        self.rawRepresentation = Data(rawRepresentation)
         fatalError()
     }
     
     public init(compactRepresentation: some DataProtocol) throws {
-
+        
         try self.init(wrapped: Bridge.ECDSA.NonRecovery.from(compactBytes: [UInt8](compactRepresentation)))
     }
     
@@ -52,34 +44,68 @@ public struct ECDSASignatureNonRecoverable: Sendable, Hashable {
             wrapped: Bridge.ECDSA.NonRecovery.from(derRepresentation: [UInt8](derRepresentation))
         )
     }
-    
-    public func compactRepresentation() throws -> Data {
-        try Bridge.ECDSA.NonRecovery.compact(wrapped)
-    }
+}
+
+// MARK: Serialize
+extension ECDSASignatureNonRecoverable {
     
     internal var rawRepresentation: Data {
         Data(wrapped.bytes)
     }
     
+    public func compactRepresentation() throws -> Data {
+        try Bridge.ECDSA.NonRecovery.compact(wrapped)
+    }
+    
     public func derRepresentation() throws -> Data {
         try Bridge.ECDSA.NonRecovery.der(wrapped)
     }
-    
+}
+
+
+// MARK: Recover
+extension ECDSASignatureNonRecoverable {
     public func recoverPublicKey(
         recoveryID: ECDSASignatureRecoverable.RecoveryID,
         message: some DataProtocol
     ) throws -> K1.PublicKey {
         try K1.PublicKey(
-            wrapped:  Bridge.ECDSA.NonRecovery.recoverPublicKey(self.wrapped, recoveryID: recoveryID.rawValue, message: [UInt8](message))
+            wrapped: Bridge.ECDSA.NonRecovery.recoverPublicKey(
+                self.wrapped,
+                recoveryID: recoveryID.rawValue,
+                message: [UInt8](message)
+            )
         )
-       
+        
     }
     
-
+    
 }
 
 extension ECDSASignatureNonRecoverable {
     internal static let byteCount = Bridge.ECDSA.Recovery.byteCount
-
+    
 }
 
+// MARK: Equatable
+extension ECDSASignatureNonRecoverable {
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.wrapped.withUnsafeBytes { lhsBytes in
+            rhs.wrapped.withUnsafeBytes { rhsBytes in
+                safeCompare(lhsBytes, rhsBytes)
+            }
+        }
+    }
+}
+
+// MARK: Hashable
+extension ECDSASignatureNonRecoverable {
+    
+    public func hash(into hasher: inout Hasher) {
+        wrapped.withUnsafeBytes {
+            hasher.combine(bytes: $0)
+        }
+    }
+    
+}
