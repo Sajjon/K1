e28e208303a694a65d291823504e61e56bef627d
Alexander Cyon <alex.cyon@gmail.com>
2023-03-20 10:56:57 +0100

    WIP

--- Sources/K1/Keys/PublicKey/PublicKey.swift
+++ Sources/K1/Keys/PublicKey/PublicKey.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import CryptoKit
 
 extension K1 {
     
@@ -34,122 +33,6 @@ extension K1.PublicKey {
     public func rawRepresentation(format: K1.Format) throws -> Data {
         try wrapped.rawRepresentation(format: format)
     }
-    
-}
-
-// MARK: Validate ECDSA Non-Recoverable
-extension K1.PublicKey {
-    
-    /// Verifies an elliptic curve digital signature algorithm (ECDSA) signature on some _hash_ over the `secp256k1` elliptic curve.
-    /// - Parameters:
-    ///   - signature: The (non-recoverable) signature to check against the _hashed_ data.
-    ///   - hashed: The _hashed_ data covered by the signature.
-    ///   - mode: Whether or not to consider malleable signatures valid.
-    /// - Returns: A Boolean value that’s true if the signature is valid for the given _hashed_ data.
-    public func isValidECDSASignature(
-        _ signature: ECDSASignatureNonRecoverable,
-        hashed: some DataProtocol,
-        mode: K1.ECDSA.ValidationMode = .default
-    ) -> Bool {
-        do {
-            return try wrapped.isValid(
-                ecdsaSignature: signature.wrapped,
-                message: [UInt8](hashed),
-                mode: mode
-            )
-        } catch {
-            return false
-        }
-    }
-    
-    /// Verifies an elliptic curve digital signature algorithm (ECDSA) signature on a digest over the `secp256k1` elliptic curve.
-    /// - Parameters:
-    ///   - signature: The (non-recoverable) signature to check against the given digest.
-    ///   - digest: The digest covered by the signature.
-    ///   - mode: Whether or not to consider malleable signatures valid.
-    /// - Returns: A Boolean value that’s true if the signature is valid for the given digest.
-    public func isValidECDSASignature(
-        _ signature: ECDSASignatureNonRecoverable,
-        digest: some Digest,
-        mode: K1.ECDSA.ValidationMode = .default
-    ) -> Bool {
-        isValidECDSASignature(
-            signature,
-            hashed: Data(digest),
-            mode: mode
-        )
-    }
-
-}
-
-// MARK: Validate ECDSA Recoverable
-extension K1.PublicKey {
-    
-    /// Converts a recoverable ECDSA signature to
-    /// non-recoverable and validates it against
-    /// a `SHA256` hashed messages for this public key.
-    public func isValidECDSASignature(
-        _ signature: ECDSASignatureRecoverable,
-        hashed: some DataProtocol,
-        mode: K1.ECDSA.ValidationMode = .default
-    ) -> Bool {
-        do {
-            return try isValidECDSASignature(
-                signature.nonRecoverable(),
-                hashed: hashed,
-                mode: mode
-            )
-        } catch {
-            return false
-        }
-    }
-    
-    /// Converts a recoverable ECDSA signature to
-    /// non-recoverable and validates it against
-    /// a `SHA256` hashed messages for this public key.
-    public func isValidECDSASignature(
-        _ signature: ECDSASignatureRecoverable,
-        digest: some Digest,
-        mode: K1.ECDSA.ValidationMode = .default
-    ) -> Bool {
-        do {
-            return try isValidECDSASignature(
-                signature.nonRecoverable(),
-                digest: digest,
-                mode: mode
-            )
-        } catch {
-            return false
-        }
-    }
-
-}
-
-// MARK: Validate Schnorr Signatures
-extension K1.PublicKey {
-    
-    
-    public func isValidSchnorrSignature(
-        _ signature: SchnorrSignature,
-        hashed: some DataProtocol
-    ) -> Bool {
-        do {
-            return try wrapped.isValid(
-                schnorrSignature: signature.wrapped,
-                message: [UInt8](hashed)
-            )
-        } catch {
-            return false
-        }
-    }
-    
-    public func isValidSchnorrSignature(
-        _ signature: SchnorrSignature,
-        digest: some Digest
-    ) -> Bool {
-        isValidSchnorrSignature(signature, hashed: Data(digest))
-    }
-
 }
 
 // MARK: Equatable
