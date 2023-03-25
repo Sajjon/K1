bff943091acefd5f7325ba7cdbe9a5e180902bea
Alexander Cyon <alex.cyon@gmail.com>
2023-03-25 14:42:19 +0100

    swiftformat Misc

--- Sources/K1/Support/Misc/Curve.swift
+++ Sources/K1/Support/Misc/Curve.swift
@@ -1,24 +1,16 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alexander Cyon on 2023-03-19.
-//
-
 import Foundation
 
 // MARK: - Curve
-
 /// Details about the elliptic curve `secp256k1`.
 enum Curve {}
 
 // MARK: - FiniteField
 extension Curve {
-    /// The finite field of the secp256k1 curve.
-    enum Field {}
+	/// The finite field of the secp256k1 curve.
+	enum Field {}
 }
 
 extension Curve.Field {
-    /// Finite field members are 256 bits large, i.e. 32 bytes.
-    static let byteCount = 32
+	/// Finite field members are 256 bits large, i.e. 32 bytes.
+	static let byteCount = 32
 }
