b134d4312500b9ead5c261c29135e8c8b0f5c78c
Alexander Cyon <alex.cyon@gmail.com>
2023-03-25 14:45:08 +0100

    swiftformat Sources/K1/K1/Keys

--- Sources/K1/K1/Keys/PublicKey/PublicKeyImplementation.swift
+++ Sources/K1/K1/Keys/PublicKey/PublicKeyImplementation.swift
@@ -1,147 +1,134 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alexander Cyon on 2022-01-27.
-//
-
 import Foundation
 
+// MARK: - K1PublicKeyProtocol
 public protocol K1PublicKeyProtocol: K1KeyPortable {
-    init(compressedRepresentation: some ContiguousBytes) throws
-    var compressedRepresentation: Data { get }
+	init(compressedRepresentation: some ContiguousBytes) throws
+	var compressedRepresentation: Data { get }
 }
 
 extension K1 {
-    
-    struct _PublicKeyImplementation: Sendable, Hashable, K1PublicKeyProtocol {
-        
-        typealias Wrapped = FFI.PublicKey.Wrapped
-        internal let wrapped: Wrapped
-        
-        internal init(wrapped: Wrapped) {
-            self.wrapped = wrapped
-        }
-    }
+	struct _PublicKeyImplementation: Sendable, Hashable, K1PublicKeyProtocol {
+		typealias Wrapped = FFI.PublicKey.Wrapped
+		internal let wrapped: Wrapped
+
+		internal init(wrapped: Wrapped) {
+			self.wrapped = wrapped
+		}
+	}
 }
 
 // MARK: Init
 extension K1._PublicKeyImplementation {
-    
+	/// `04 || X || Y` (65 bytes)
+	static let x963ByteCount = FFI.PublicKey.x963ByteCount
 
-    /// `04 || X || Y` (65 bytes)
-    static let x963ByteCount = FFI.PublicKey.x963ByteCount
-    
-    /// `X || Y` (64 bytes)
-    static let rawByteCount = FFI.PublicKey.rawByteCount
+	/// `X || Y` (64 bytes)
+	static let rawByteCount = FFI.PublicKey.rawByteCount
 
-    /// `02|03 || X` (33 bytes)
-    static let compressedByteCount = FFI.PublicKey.compressedByteCount
-    
-    /// `X || Y` (64 bytes)
-    init(rawRepresentation: some ContiguousBytes) throws {
-        try self.init(
-            wrapped: FFI.PublicKey.deserialize(rawRepresentation: rawRepresentation)
-        )
-    }
-    
-    /// `04 || X || Y` (65 bytes)
-    init(x963Representation: some ContiguousBytes) throws {
-        try self.init(
-            wrapped: FFI.PublicKey.deserialize(x963Representation: x963Representation)
-        )
-    }
-    
-    /// `DER`
-    init(derRepresentation: some RandomAccessCollection<UInt8>) throws {
-        let bytes = [UInt8](derRepresentation)
-        let parsed = try ASN1.SubjectPublicKeyInfo(asn1Encoded: bytes)
-        self = try .init(x963Representation: parsed.key)
-    }
-    
-    /// `02|03 || X` (33 bytes)
-    init(compressedRepresentation: some ContiguousBytes) throws {
-        try self.init(
-            wrapped: FFI.PublicKey.deserialize(compressedRepresentation: compressedRepresentation)
-        )
-    }
+	/// `02|03 || X` (33 bytes)
+	static let compressedByteCount = FFI.PublicKey.compressedByteCount
 
-    /// Creates a `secp256k1` key from a Privacy-Enhanced Mail (PEM) representation.
-    init(pemRepresentation: String) throws {
-        let pem = try ASN1.PEMDocument(pemString: pemRepresentation)
-        guard pem.type == Self.pemType else {
-            throw K1.Error.invalidPEMDocument
-        }
-        self = try .init(derRepresentation: pem.derBytes)
-    }
+	/// `X || Y` (64 bytes)
+	init(rawRepresentation: some ContiguousBytes) throws {
+		try self.init(
+			wrapped: FFI.PublicKey.deserialize(rawRepresentation: rawRepresentation)
+		)
+	}
 
+	/// `04 || X || Y` (65 bytes)
+	init(x963Representation: some ContiguousBytes) throws {
+		try self.init(
+			wrapped: FFI.PublicKey.deserialize(x963Representation: x963Representation)
+		)
+	}
+
+	/// `DER`
+	init(derRepresentation: some RandomAccessCollection<UInt8>) throws {
+		let bytes = [UInt8](derRepresentation)
+		let parsed = try ASN1.SubjectPublicKeyInfo(asn1Encoded: bytes)
+		self = try .init(x963Representation: parsed.key)
+	}
+
+	/// `02|03 || X` (33 bytes)
+	init(compressedRepresentation: some ContiguousBytes) throws {
+		try self.init(
+			wrapped: FFI.PublicKey.deserialize(compressedRepresentation: compressedRepresentation)
+		)
+	}
+
+	/// Creates a `secp256k1` key from a Privacy-Enhanced Mail (PEM) representation.
+	init(pemRepresentation: String) throws {
+		let pem = try ASN1.PEMDocument(pemString: pemRepresentation)
+		guard pem.type == Self.pemType else {
+			throw K1.Error.invalidPEMDocument
+		}
+		self = try .init(derRepresentation: pem.derBytes)
+	}
 }
 
 extension K1._PublicKeyImplementation {
-    static let pemType = "PUBLIC KEY"
+	static let pemType = "PUBLIC KEY"
 }
 
 // MARK: Serialize
 extension K1._PublicKeyImplementation {
-    
-    /// `X || Y` (64 bytes)
-    var rawRepresentation: Data {
-        Data(x963Representation.dropFirst())
-    }
-    
-    
-    /// `04 || X || Y` (65 bytes)
-    var x963Representation: Data {
-        try! FFI.PublicKey.serialize(wrapped, format: .uncompressed)
-    }
-    
-    /// `02|03 || X` (33 bytes)
-    var compressedRepresentation: Data {
-        try! FFI.PublicKey.serialize(wrapped, format: .compressed)
-    }
-    
-    /// `DER`
-    var derRepresentation: Data {
-        let spki = ASN1.SubjectPublicKeyInfo(
-            algorithmIdentifier: .secp256k1,
-            key: Array(self.x963Representation)
-        )
-        var serializer = ASN1.Serializer()
-        
-        // Serializing these keys can't throw
-        try! serializer.serialize(spki)
-        return Data(serializer.serializedBytes)
-    }
-    
-    /// A Privacy-Enhanced Mail (PEM) representation of the public key.
-    public var pemRepresentation: String {
-        let pemDocument = ASN1.PEMDocument(type: Self.pemType, derBytes: self.derRepresentation)
-        return pemDocument.pemString
-    }
+	/// `X || Y` (64 bytes)
+	var rawRepresentation: Data {
+		Data(x963Representation.dropFirst())
+	}
+
+	/// `04 || X || Y` (65 bytes)
+	var x963Representation: Data {
+		try! FFI.PublicKey.serialize(wrapped, format: .uncompressed)
+	}
+
+	/// `02|03 || X` (33 bytes)
+	var compressedRepresentation: Data {
+		try! FFI.PublicKey.serialize(wrapped, format: .compressed)
+	}
+
+	/// `DER`
+	var derRepresentation: Data {
+		let spki = ASN1.SubjectPublicKeyInfo(
+			algorithmIdentifier: .secp256k1,
+			key: Array(self.x963Representation)
+		)
+		var serializer = ASN1.Serializer()
+
+		// Serializing these keys can't throw
+		try! serializer.serialize(spki)
+		return Data(serializer.serializedBytes)
+	}
+
+	/// A Privacy-Enhanced Mail (PEM) representation of the public key.
+	public var pemRepresentation: String {
+		let pemDocument = ASN1.PEMDocument(type: Self.pemType, derBytes: self.derRepresentation)
+		return pemDocument.pemString
+	}
 }
 
 // MARK: Equatable
 extension K1._PublicKeyImplementation {
-    static func == (lhsSelf: Self, rhsSelf: Self) -> Bool {
-        let lhs = lhsSelf.wrapped
-        let rhs = rhsSelf.wrapped
-        do {
-            return try lhs.compare(to: rhs)
-        } catch {
-            return lhs.withUnsafeBytes { lhsBytes in
-                rhs.withUnsafeBytes { rhsBytes in
-                    safeCompare(lhsBytes, rhsBytes)
-                }
-            }
-        }
-    }
+	static func == (lhsSelf: Self, rhsSelf: Self) -> Bool {
+		let lhs = lhsSelf.wrapped
+		let rhs = rhsSelf.wrapped
+		do {
+			return try lhs.compare(to: rhs)
+		} catch {
+			return lhs.withUnsafeBytes { lhsBytes in
+				rhs.withUnsafeBytes { rhsBytes in
+					safeCompare(lhsBytes, rhsBytes)
+				}
+			}
+		}
+	}
 }
 
 // MARK: Hashable
 extension K1._PublicKeyImplementation {
-    func hash(into hasher: inout Hasher) {
-        wrapped.withUnsafeBytes {
-            hasher.combine(bytes: $0)
-        }
-    }
+	func hash(into hasher: inout Hasher) {
+		wrapped.withUnsafeBytes {
+			hasher.combine(bytes: $0)
+		}
+	}
 }
--- Sources/K1/K1/Keys/PublicKey/PublicKeyOf+Feature.swift
+++ Sources/K1/K1/Keys/PublicKey/PublicKeyOf+Feature.swift
@@ -1,58 +1,50 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alexander Cyon on 2023-03-24.
-//
-
 import Foundation
 
 // TODO: Replace typealiases and this single existential to use existentials generated with GYB.
 public struct PublicKeyOf<Feature>: Sendable, Hashable, K1PublicKeyProtocol {
-    
-    public init(rawRepresentation: some ContiguousBytes) throws {
-        try self.init(impl: .init(rawRepresentation: rawRepresentation))
-    }
-    
-    public init(compressedRepresentation: some ContiguousBytes) throws {
-        try self.init(impl: .init(compressedRepresentation: compressedRepresentation))
-    }
-    
-    public init(x963Representation: some ContiguousBytes) throws {
-        try self.init(impl: .init(x963Representation: x963Representation))
-    }
-    
-    public init(derRepresentation: some RandomAccessCollection<UInt8>) throws {
-        try self.init(impl: .init(derRepresentation: derRepresentation))
-    }
-    
-    public init(pemRepresentation: String) throws {
-        try self.init(impl: .init(pemRepresentation: pemRepresentation))
-    }
-    
-    public var rawRepresentation: Data {
-        impl.rawRepresentation
-    }
-    
-    public var x963Representation: Data {
-        impl.x963Representation
-    }
-    
-    public var derRepresentation: Data {
-        impl.derRepresentation
-    }
-    
-    public var compressedRepresentation: Data {
-        impl.compressedRepresentation
-    }
-    
-    public var pemRepresentation: String {
-        impl.pemRepresentation
-    }
-    
-    typealias Impl = K1._PublicKeyImplementation
-    internal let impl: Impl
-    internal init(impl: Impl) {
-        self.impl = impl
-    }
+	public init(rawRepresentation: some ContiguousBytes) throws {
+		try self.init(impl: .init(rawRepresentation: rawRepresentation))
+	}
+
+	public init(compressedRepresentation: some ContiguousBytes) throws {
+		try self.init(impl: .init(compressedRepresentation: compressedRepresentation))
+	}
+
+	public init(x963Representation: some ContiguousBytes) throws {
+		try self.init(impl: .init(x963Representation: x963Representation))
+	}
+
+	public init(derRepresentation: some RandomAccessCollection<UInt8>) throws {
+		try self.init(impl: .init(derRepresentation: derRepresentation))
+	}
+
+	public init(pemRepresentation: String) throws {
+		try self.init(impl: .init(pemRepresentation: pemRepresentation))
+	}
+
+	public var rawRepresentation: Data {
+		impl.rawRepresentation
+	}
+
+	public var x963Representation: Data {
+		impl.x963Representation
+	}
+
+	public var derRepresentation: Data {
+		impl.derRepresentation
+	}
+
+	public var compressedRepresentation: Data {
+		impl.compressedRepresentation
+	}
+
+	public var pemRepresentation: String {
+		impl.pemRepresentation
+	}
+
+	typealias Impl = K1._PublicKeyImplementation
+	internal let impl: Impl
+	internal init(impl: Impl) {
+		self.impl = impl
+	}
 }
