dce86ccfeb46d1e02f4fc39e462736c89e600c47
Alexander Cyon <alex.cyon@gmail.com>
2023-03-26 15:48:55 +0200

    Rename CryptoKitASN1Error to K1.ASN1Error

--- Sources/K1/Support/ThirdyParty/swift-crypto/ASN1Error.swift
+++ Sources/K1/Support/ThirdyParty/swift-crypto/ASN1Error.swift
@@ -13,31 +13,35 @@
 //===----------------------------------------------------------------------===//
 
 // FROM: https://github.com/apple/swift-crypto/blob/5d8b14d22701c8394ad8cd5b297f8ffd9a6d3d4a/Sources/Crypto/CryptoKitErrors.swift#L34
+// MODIFICATIONS: Moved to `K1` namespace, i.e. `CryptoKitASN1Error` -> `K1.ASN1Error`.
+internal typealias CryptoKitASN1Error = K1.ASN1Error
 
-/// Errors encountered when parsing ASN.1 formatted keys.
-public enum CryptoKitASN1Error: Error {
-    /// The ASN.1 tag for this field is invalid or unsupported.
-    case invalidFieldIdentifier
-
-    /// The ASN.1 tag for the parsed field does not match the required format.
-    case unexpectedFieldType
-
-    /// An invalid ASN.1 object identifier was encountered.
-    case invalidObjectIdentifier
-
-    /// The format of the parsed ASN.1 object does not match the format required for the data type
-    /// being decoded.
-    case invalidASN1Object
-
-    /// An ASN.1 integer was decoded that does not use the minimum number of bytes for its encoding.
-    case invalidASN1IntegerEncoding
-
-    /// An ASN.1 field was truncated and could not be decoded.
-    case truncatedASN1Field
-
-    /// The encoding used for the field length is not supported.
-    case unsupportedFieldLength
-
-    /// It was not possible to parse a string as a PEM document.
-    case invalidPEMDocument
+extension K1 {
+	/// Errors encountered when parsing ASN.1 formatted keys.
+	public enum ASN1Error: Swift.Error {
+		/// The ASN.1 tag for this field is invalid or unsupported.
+		case invalidFieldIdentifier
+		
+		/// The ASN.1 tag for the parsed field does not match the required format.
+		case unexpectedFieldType
+		
+		/// An invalid ASN.1 object identifier was encountered.
+		case invalidObjectIdentifier
+		
+		/// The format of the parsed ASN.1 object does not match the format required for the data type
+		/// being decoded.
+		case invalidASN1Object
+		
+		/// An ASN.1 integer was decoded that does not use the minimum number of bytes for its encoding.
+		case invalidASN1IntegerEncoding
+		
+		/// An ASN.1 field was truncated and could not be decoded.
+		case truncatedASN1Field
+		
+		/// The encoding used for the field length is not supported.
+		case unsupportedFieldLength
+		
+		/// It was not possible to parse a string as a PEM document.
+		case invalidPEMDocument
+	}
 }
